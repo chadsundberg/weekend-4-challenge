17906a7563bf0095457e6b0bb4057a9c8ee1bf43
Chad Sundberg <chadsundberg@gmail.com>
2017-03-04 16:03:11 -0600

    submit is close and delete is working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,57 @@
 $(document).ready(function(){
   console.log('jquery is up!');
+  getTaskData();
+  function getTaskData() {
+    $.ajax({
+      type: 'GET',
+      url: '/tasks',
+      success: function(response) {
+        console.log('response', response);
+        $('#taskList').empty();
+        for (var i = 0; i < response.length; i++) {
+          var currentTask = response[i];
+          var $newTask = $('<tr>');
+          $newTask.data('id', currentTask.id);
+          $newTask.append('<td>' + currentTask.task_name + '</td>');
+          $newTask.append('<td><button class="completeButton">Completed</button>' + currentTask.task_completed + '</td>');
+          $newTask.append('<td><button class="deleteButton">Delete</button></td>');
+          $('#taskList').prepend($newTask);
+        }
+      }
+    });
+  }
 
+  $('#newTaskForm').on('submit', function(event){
+      event.preventDefault();
+      var newTaskObject = {};
+      var formFields = $(this).serializeArray();
 
+      formFields.forEach(function (field) {
+        newTaskObject[field.name] = field.value;
+      });
+
+      $.ajax({
+        type: 'POST',
+        url: '/tasks/new',
+        data: newTaskObject,
+        success: function(response){
+          console.log(response);
+          getTaskData();
+          $('#newTaskForm > input').val('');
+        }
+      });
+    });
+
+    $('#taskList').on('click', '.deleteButton', function(){
+    var idOfTaskToDelete = $(this).parent().parent().data().id;
+    console.log('the id to delete is: ', idOfTaskToDelete);
+    $.ajax({
+      type: 'DELETE',
+      url: '/tasks/delete/' + idOfTaskToDelete, // optional parameter
+      success: function(response) {
+        console.log(response);
+        getTaskData();
+      }
+    })
+  });
 });
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,18 +2,78 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var config = {
-  database: 'phi', // the name of the database
-  host: 'localhost', // where is your database
-  port: 5432, // the port number for your database
-  max: 10, // how many connections at one time
-  idleTimeoutMillis: 30000 // 30 seconds to try to connect
+  database: 'phi',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
 };
 
 var pool = new pg.Pool(config);
 
+router.get('/', function(req, res){
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      client.query('SELECT * FROM "tasks";', function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+});
 
+router.post('/new', function(req, res){
+  var newTask = req.body;
 
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      client.query('INSERT INTO tasks (task_name, task_completed) VALUES ($1, $2);',
+        [newTask.task_name, newTask.task_completed],
+        function(errorMakingQuery, result){
+          done();
+          if(errorMakingQuery) {
+            console.log('Error making the database query: ', errorMakingQuery);
+            res.sendStatus(500);
+          } else {
+            res.sendStatus(201);
+          }
+        });
+    }
+  });
+});
 
-
+router.delete('/delete/:id', function(req, res){
+  var taskId = req.params.id;
+  console.log('book of id to delete: ', taskId);
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      client.query('DELETE FROM tasks WHERE id=$1;', // $1 tells the server to look to the array for [bookId] ajax talks to SQL to do a query
+        [taskId], // this is the array of things that replaces the $1, $2, $3 in the query
+        function(errorMakingQuery, result){
+          done();
+          if(errorMakingQuery) {
+            console.log('Error making the database query: ', errorMakingQuery);
+            res.sendStatus(500);
+          } else {
+            res.sendStatus(201);
+          }
+        });
+    }
+  });
+});
 
 module.exports = router;
